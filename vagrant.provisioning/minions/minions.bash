0134a224dad339a6cdead52e8dc7fe8bc62dcd98
i90rr <i90rr.i90rr@gmail.com>
2017-10-06 16:23:10 -0300

    + python3-pip, sqlalchemy

--- vagrant.provisioning/minions/minions.bash
+++ vagrant.provisioning/minions/minions.bash
@@ -5,7 +5,11 @@ _hostname=$(hostname)
 set -ex
 echo "Installing salt-minion on host: $_hostname..."
 sudo apt-get update
-sudo apt-get -y install mg tmux salt-minion
+sudo apt-get -y install mg tmux salt-minion python3-pip
+
+# pip3 install sqlalchemy
+python3 -m pip install
+
 echo -e "\n# Salt master\n192.168.0.5     salt" | sudo tee -a /etc/hosts > /dev/null
 echo "Starting salt-minion in host: $_hostname..."
 sudo systemctl start salt-minion
